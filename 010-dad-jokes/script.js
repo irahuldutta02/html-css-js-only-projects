566078c766dc0f08c22d4823be8d36f027c68595
irahuldutta02 <rdtech2002@gmail.com>
2023-11-03 14:13:41 +0530

    .

--- 010-dad-jokes/script.js
+++ 010-dad-jokes/script.js
@@ -3,17 +3,35 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 generateJoke();
 
-function generateJoke() {
+
+// using .then 
+// ---------------
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+//   fetch("https://icanhazdadjoke.com/", config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
+
+
+// using async await
+// -----------------
+async function generateJoke() {
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
-  fetch("https://icanhazdadjoke.com/", config)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeEl.innerHTML = data.joke;
-    });
+  const res = await fetch("https://icanhazdadjoke.com/", config);
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
 }
 
 jokeBtn.addEventListener("click", generateJoke);
